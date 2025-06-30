7b3323d4ba6cfdc434afb3d2f7271e9d1b931547
dsfreehand <daniel.sisson@live.com>
2025-06-30 10:20:43 -0400

    Updated vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
-// client/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Digital_Resume/',
   plugins: [react()],
   build: {
     outDir: 'dist',
